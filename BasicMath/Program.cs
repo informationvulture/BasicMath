d6e61b8b4fb2d0e913928276b1916b7cb162fd2e
informationvulture <schoolemailnm1@gmail.com>
2023-05-13 17:15:29 -0700

    Add MCQ functionality

--- BasicMath/Program.cs
+++ BasicMath/Program.cs
@@ -1,13 +1,9 @@
 ﻿using System;
 using System.Drawing;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace BasicMath
 {
-
-
-
     public class Program
     {
         static void Main()
@@ -16,46 +12,64 @@ namespace BasicMath
         }
     }
 
-
-
     public class MultiplicationForm : Form
     {
         private Random random = new Random();
         private Label questionLabel = new Label();
-        private TextBox answerTextBox = new TextBox();
-        private Button submitButton = new Button();
+        private Label answerLabel1 = new Label();
+        private Label answerLabel2 = new Label();
+        private Label answerLabel3 = new Label();
+        private Label answerLabel4 = new Label();
         private Label resultLabel = new Label();
+        private Label scoreLabel = new Label();
         private Label timerLabel = new Label();
         private Timer timer = new Timer();
+        private Button nextButton = new Button();
         private DateTime startTime;
-        private Label correctLabel = new Label();
-        private Label incorrectLabel = new Label();
-        private int correctScore = 0;
-        private int incorrectScore = 0;
-
+        private int correctAnswers = 0;
+        private int incorrectAnswers = 0;
 
         public MultiplicationForm()
         {
-            this.Icon = Properties.Resources.app_icon_122;
+            Size = new Size(800, 600);
+            Icon = Properties.Resources.app_icon_122;
             questionLabel.Location = new Point(10, 10);
             questionLabel.Width = 400;
             Controls.Add(questionLabel);
 
-            answerTextBox.Location = new Point(10, 40);
-            Controls.Add(answerTextBox);
+            answerLabel1.Location = new Point(10, 40);
+            answerLabel1.Click += new EventHandler(AnswerLabel_Click);
+            Controls.Add(answerLabel1);
 
-            submitButton.Text = "Submit";
-            submitButton.Location = new Point(10, 70);
-            submitButton.Click += new EventHandler(SubmitButton_Click);
-            Controls.Add(submitButton);
+            answerLabel2.Location = new Point(10, 70);
+            answerLabel2.Click += new EventHandler(AnswerLabel_Click);
+            Controls.Add(answerLabel2);
 
-            resultLabel.Location = new Point(10, 100);
+            answerLabel3.Location = new Point(10, 100);
+            answerLabel3.Click += new EventHandler(AnswerLabel_Click);
+            Controls.Add(answerLabel3);
+
+            answerLabel4.Location = new Point(10, 130);
+            answerLabel4.Click += new EventHandler(AnswerLabel_Click);
+            Controls.Add(answerLabel4);
+
+            resultLabel.Location = new Point(10, 160);
             resultLabel.Width = 400;
             Controls.Add(resultLabel);
 
-            timerLabel.Location = new Point(10, 130);
+            scoreLabel.Location = new Point(10, 190);
+            scoreLabel.Width = 400;
+            Controls.Add(scoreLabel);
+
+            timerLabel.Location = new Point(10, 220);
             timerLabel.Width = 400;
             Controls.Add(timerLabel);
+
+            nextButton.Text = "Next";
+            nextButton.Location = new Point(10, 250);
+            nextButton.Click += new EventHandler(NextButton_Click);
+            Controls.Add(nextButton);
+            nextButton.Visible = false;
 
             timer.Interval = 1000; // one second
             timer.Tick += new EventHandler(Timer_Tick);
@@ -63,73 +77,77 @@ namespace BasicMath
 
             startTime = DateTime.Now;
 
-            correctLabel.Location = new Point(10, 160);
-            correctLabel.Width = 200;
-            Controls.Add(correctLabel);
-
-            incorrectLabel.Location = new Point(10, 190);
-            incorrectLabel.Width = 200;
-            Controls.Add(incorrectLabel);
-
-            UpdateScores();
-
             GenerateNewProblem();
-
-
         }
 
-        private void UpdateScores()
-        {
-            correctLabel.Text = $"Correct answers: {correctScore}";
-            incorrectLabel.Text = $"Incorrect answers: {incorrectScore}";
-        }
         private void GenerateNewProblem()
         {
+            nextButton.Visible = false;
             int number1 = random.Next(100, 1000);
             int number2 = random.Next(10, 100);
+            int correctAnswer = number1 * number2;
 
             questionLabel.Text = $"Please multiply these two numbers: {number1} and {number2}";
-            questionLabel.Tag = number1 * number2;
+            questionLabel.Tag = correctAnswer;
 
-            answerTextBox.Text = string.Empty;
-            resultLabel.Text = string.Empty;
-        }
-
-        private async void SubmitButton_Click(object sender, EventArgs e)
-        {
-            int userAnswer;
-            if (int.TryParse(answerTextBox.Text, out userAnswer))
+            // Generate random answers
+            int[] answers = new int[4];
+            answers[0] = correctAnswer;
+            for (int i = 1; i < 4; i++)
             {
-                if ((int)questionLabel.Tag == userAnswer)
+                answers[i] = random.Next(1000, 100000);
+            }
+
+            // Shuffle the answers
+            for (int i = answers.Length - 1; i > 0; i--)
+            {
+                int j = random.Next(i + 1);
+                int temp = answers[i];
+                answers[i] = answers[j];
+                answers[j] = temp;
+            }
+
+                answerLabel1.Text = answers[0].ToString();
+                answerLabel1.Tag = answers[0] == correctAnswer;
+
+                answerLabel2.Text = answers[1].ToString();
+                answerLabel2.Tag = answers[1] == correctAnswer;
+
+                answerLabel3.Text = answers[2].ToString();
+                answerLabel3.Tag = answers[2] == correctAnswer;
+
+                answerLabel4.Text = answers[3].ToString();
+                answerLabel4.Tag = answers[3] == correctAnswer;
+
+                resultLabel.Text = string.Empty;
+            }
+
+    private void AnswerLabel_Click(object sender, EventArgs e)
+            {
+                Label clickedLabel = (Label)sender;
+                if ((bool)clickedLabel.Tag)
                 {
-                    correctScore++;
-                    UpdateScores();
-                    GenerateNewProblem();
+                    resultLabel.Text = "Correct! Well done.";
+                    correctAnswers++;
                 }
                 else
                 {
                     resultLabel.Text = $"Sorry, that's incorrect. The correct answer is {(int)questionLabel.Tag}.";
-                    await Task.Delay(2000);
-                    incorrectScore++;
-                    UpdateScores();
-                    GenerateNewProblem();
+                    incorrectAnswers++;
+                    nextButton.Visible = true;
                 }
+                scoreLabel.Text = $"Score: {correctAnswers} correct, {incorrectAnswers} incorrect";
             }
-            else
+
+            private void NextButton_Click(object sender, EventArgs e)
             {
-                resultLabel.Text = "Invalid input. Please enter a number.";
+                GenerateNewProblem();
             }
 
-
-        }
-
-        private void Timer_Tick(object sender, EventArgs e)
-        {
-            TimeSpan elapsed = DateTime.Now - startTime;
-            timerLabel.Text = $"Elapsed time: {elapsed.ToString(@"hh\:mm\:ss")}";
+            private void Timer_Tick(object sender, EventArgs e)
+            {
+                TimeSpan elapsed = DateTime.Now - startTime;
+                timerLabel.Text = $"Elapsed time: {elapsed.ToString(@"hh\:mm\:ss")}";
+            }
         }
     }
-
-
-
-}
